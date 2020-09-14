cef31dd32c9b5c41e6ed103fe29c0a2e48cdde55
Murat KURT <paralaks@gmail.com>
2020-09-14 17:12:34 -0400

    Implemented Stack based on LinkedList

---
+++ src/paralaks_gmail_com/data_structures_algorithms/Stack.java
@@ -0,0 +1,50 @@
+package paralaks_gmail_com.data_structures_algorithms;
+
+import java.util.Iterator;
+
+public class Stack<T> implements Collection<T> {
+  private final LinkedList<T> list = new LinkedList<>();
+
+  public void push(T value) {
+    list.addLast(value);
+  }
+
+  public T pop() {
+    return list.removeLast();
+  }
+
+  @Override
+  public void clear() {
+    list.clear();
+  }
+
+  @Override
+  public void add(T value) {
+    push(value);
+  }
+
+  @Override
+  public T peek() {
+    return list.peekLast();
+  }
+
+  @Override
+  public boolean contains(T value) {
+    return list.contains(value);
+  }
+
+  @Override
+  public T remove() {
+    return pop();
+  }
+
+  @Override
+  public int size() {
+    return list.size();
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    return list.iterator();
+  }
+}
